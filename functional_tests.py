54c7e51c14c3310e91684427a5582e16b067ecba
Tyler Collins <tjscollins@gmail.com>
2017-04-21 18:39:57 +1000

    Refactor functional_tests.py to eliminate duplicate code from tests

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Joe has heard about a cool new online to-do app.  He checks out the app's
         # homepage.
@@ -36,10 +41,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy new swingline stapler" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy new swingline stapler', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy new swingline stapler')
 
         # There is still a text box inviting Joe to add another item.  He enters
         # "Burn down office and collect insurance money"
@@ -49,10 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on Joe's list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy new swingline stapler', [row.text for row in rows])
-        self.assertIn('2: Burn down office and collect insurance money', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy new swingline stapler')
+        self.check_for_row_in_list_table('2: Burn down office and collect insurance money')
 
         # Joe wonders whether the site will remember his list.  Then he sees that
         # the site has generated a unique URL for him -- there is some explanatory
