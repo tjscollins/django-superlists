3a055f1e3f7ef9329820873f84c64b5e7e7c4946
Tyler Collins <tjscollins@gmail.com>
2017-04-22 14:40:39 +1000

    Remove voodoo sleeps in favor of loop w/ polling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -11,10 +14,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.1)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Joe has heard about a cool new online to-do app.  He checks out the app's
@@ -40,19 +51,17 @@ class NewVisitorTest(LiveServerTestCase):
         # When he hits enter, the page updates, and now the page lists:
         # "1: Buy new swingline stapler" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy new swingline stapler')
+        self.wait_for_row_in_list_table('1: Buy new swingline stapler')
 
         # There is still a text box inviting Joe to add another item.  He enters
         # "Burn down office and collect insurance money"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Burn down office and collect insurance money')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on Joe's list
-        self.check_for_row_in_list_table('1: Buy new swingline stapler')
-        self.check_for_row_in_list_table('2: Burn down office and collect insurance money')
+        self.wait_for_row_in_list_table('1: Buy new swingline stapler')
+        self.wait_for_row_in_list_table('2: Burn down office and collect insurance money')
 
         # Joe wonders whether the site will remember his list.  Then he sees that
         # the site has generated a unique URL for him -- there is some explanatory
