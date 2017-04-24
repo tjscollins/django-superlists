21f249f4255f6d58c974d1e73408dd6b5997a852
Tyler Collins <tjscollins@gmail.com>
2017-04-24 16:51:54 +1000

    URLs for forms x2, moved code in views + tests, new URL

--- lists/tests.py
+++ lists/tests.py
@@ -2,43 +2,40 @@ from django.test import TestCase
 from lists.models import Item
 
 
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/thelist/')
+
+
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/thelist')
+        response = self.client.get('/lists/thelist/')
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/lists/thelist')
+        response = self.client.get('/lists/thelist/')
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
 
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text': 'A new list item'})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/thelist')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ItemModelTest(TestCase):
 
--- superlists/urls.py
+++ superlists/urls.py
@@ -19,5 +19,6 @@ from lists import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
-    url(r'^lists/thelist', views.view_list, name='view_list')
+    url(r'^lists/new$', views.new_list, name='new_list'),
+    url(r'^lists/thelist/$', views.view_list, name='view_list')
 ]
